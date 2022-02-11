b16d6c30af8875cb2844ea03cbac9fc4099ec32a
Knitschi <knitschi.public@freenet.de>
2022-02-11 12:44:43 +0100

    Updates packages and conanfile requirements.

--- Sources/test_package_MyLib/conanfile.py
+++ Sources/test_package_MyLib/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 class MyLibTest(ConanFile):
 
     # Dependencies
-    python_requires =  "CPFConanfile/0.0.19@knitschi/development", "CPFPackageTestConanfile/0.0.4@knitschi/development",
+    python_requires =  "CPFConanfile/0.0.20@knitschi/development", "CPFPackageTestConanfile/0.0.4@knitschi/development",
     python_requires_extend = "CPFConanfile.CPFBaseConanfile", "CPFPackageTestConanfile.CPFBasePackageTestConanfile", 
 
     cpf_conanfile_module = None
